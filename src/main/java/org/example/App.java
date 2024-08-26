90cee4d7716c88c59480053a3081630f45eecd4e
Cristian Ailincai <cristian_ailincai@yahoo.com>
2024-08-26 20:29:26 +0300

    added String Pool and object comparison examples

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,35 @@
 package org.example;
 
+import org.example.competitor.vehicle.Car;
+import org.example.competitor.vehicle.Engine;
+
 public class App {
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
+        // String pool
+//        String a = "test";
+//        String b = "test";
+//        String c = new String ("test");
+//
+//        System.out.println("a == b:"+ (a == b));
+//        System.out.println("a == b:"+ (a == c));
+//        System.out.println("a.equals(c): "+ (a.equals(c)));
 
+//        Car car1 =new Car();
+//        car1.setName("Ferrari");
+//
+//        Car car2 = new Car();
+//        car2.setName("Ferrari");
+//
+//        System.out.println(car1.equals(car2));
+
+//        Engine engine1 = new Engine();
+//        engine1.setCapacity(200);
+//
+//        Engine engine2 = new Engine();
+//        engine2.setCapacity(200);
+//        System.out.println(engine1.equals(engine2));
     }
     }
 
